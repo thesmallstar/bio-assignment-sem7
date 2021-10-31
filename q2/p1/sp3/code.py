0347b984fa1163652116a47983d998941831a231
Rohini Mishra <rohini.mishra13201@gmail.com>
2021-11-01 02:14:30 +0530

    Modified Q2/P1/SP3

--- q2/p1/sp3/code.py
+++ q2/p1/sp3/code.py
@@ -1,7 +1,15 @@
 from Bio import SeqIO
-fasta_sequences = SeqIO.parse(open('../data/insulin_human.fasta'), 'fasta')
+
+def getSequencesFromFastaFile(input_file):
+    fasta_sequences = SeqIO.parse(open(input_file), 'fasta')
+    sequences = []
+    for fasta in fasta_sequences:
+        _id, sequence = fasta.id, str(fasta.seq)
+        sequences.append((_id,sequence))
+    return sequences
+
+fasta_sequences = getSequencesFromFastaFile('../data/insulin_human.fasta')
 for fasta in fasta_sequences:
-    _id, sequence = fasta.id, str(fasta.seq)
+    _id, sequence = fasta
     print("Fasta ID for Human Insulin Gene:", _id) 
     print("The number of ‘GAAT’ sequence repeats in the gene are:", sequence.count('GAAT'))
-    
