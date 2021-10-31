b00048d6df605cef7274c9162adc6a8c9f6c8571
Manthan <manthan.surkar@razorpay.com>
2021-11-01 01:48:20 +0530

    added code for Hbonds calculation

--- q2/p1/sp2/code.py
+++ q2/p1/sp2/code.py
@@ -16,15 +16,32 @@ dataPath = "../data/"
 
 
 def getHbondsInSequence(sequence):
+    countA = 0
+    countG = 0
+    countC = 0
+    countT = 0
+
+    for neuliotide in sequence:
+        if neuliotide == 'A':
+            countA = countA + 1
+        if neuliotide == 'G':
+            countG = countG + 1
+        if neuliotide == 'C':
+            countC = countC + 1
+        if neuliotide == 'T':
+            countT = countT + 1
+
+    return (countG+countC)*3 + (countA + countT)*2
 
 
 def processFile(fileName):
-    print("Processing File: " + fileName + "\n")
+    print("Processing File: " + fileName)
     sequences = getSequencesFromFastaFile(dataPath+fileName)
-    print("Found " + str(len(sequences)) + " Sequences \n")
+    print("Found " + str(len(sequences)) + " Sequences")
     count = 1
     for sequence in sequences:
-        print("Number of H bonds in Sequence: " str(count) + "is" + str(getHbondsInSequence(sequence)))
+        print("Number of H bonds in Sequence: " + str(count) +
+              " is " + str(getHbondsInSequence(sequence)) + "\n")
 
 
 files = [f for f in listdir(dataPath) if isfile(join(dataPath, f))]
