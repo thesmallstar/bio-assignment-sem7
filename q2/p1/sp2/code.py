a7e7c1a587d3f1acde444f96076bff448b8d7fb1
Manthan <manthan.surkar@razorpay.com>
2021-11-01 01:42:36 +0530

    incomplte code of H bonds

--- q2/p1/sp2/code.py
+++ q2/p1/sp2/code.py
@@ -1,9 +1,32 @@
 from Bio import SeqIO
+from os import listdir
+from os.path import isfile, join
 
-input_file = "input.fasta"
-fasta_sequences = SeqIO.parse(open(input_file), 'fasta')
 
-with open(output_file) as out_file:
+def getSequencesFromFastaFile(input_file):
+    fasta_sequences = SeqIO.parse(open(input_file), 'fasta')
+    sequences = []
     for fasta in fasta_sequences:
         name, sequence = fasta.id, str(fasta.seq)
-        print(sequence)
+        sequences.append(sequence)
+    return sequences
+
+
+dataPath = "../data/"
+
+
+def getHbondsInSequence(sequence):
+
+
+def processFile(fileName):
+    print("Processing File: " + fileName + "\n")
+    sequences = getSequencesFromFastaFile(dataPath+fileName)
+    print("Found " + str(len(sequences)) + " Sequences \n")
+    count = 1
+    for sequence in sequences:
+        print("Number of H bonds in Sequence: " str(count) + "is" + str(getHbondsInSequence(sequence)))
+
+
+files = [f for f in listdir(dataPath) if isfile(join(dataPath, f))]
+for file in files:
+    processFile(file)
