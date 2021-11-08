92ec3c91b44925c680bc548edd0b1389fdcaa075
minnie007 <46608967+minnie007@users.noreply.github.com>
2021-11-08 17:02:57 +0530

    Update code.py

--- q2/p2/code.py
+++ q2/p2/code.py
@@ -1,59 +1,62 @@
-inputfile = "dna_sequence.txt"
+inputfile ="dna_sequence_2.txt"
 f = open(inputfile, "r")
 dna_seq = f.read()
 dna_seq = dna_seq.replace("\n", "")
-print("\n.............DNA SEQUENCE.............")
-print(dna_seq)
+dna_seq = dna_seq.replace("N", "")
+print ("\n.............DNA SEQUENCE.............\n")
+print (dna_seq)
 
 complement = {'A': 'T', 'C': 'G', 'G': 'C', 'T': 'A'}
 bases = list(dna_seq)
 bases = [complement[base] for base in bases]
 comp_seq = ''.join(bases)
-print("\n.............COMPLEMENTARY SEQUENCE.............")
+print ("\n.............COMPLEMENTARY SEQUENCE.............\n")
 print(comp_seq)
 
 rna = {'A': 'U', 'C': 'G', 'G': 'C', 'T': 'A'}
 bases = list(comp_seq)
 bases = [rna[base] for base in bases]
 rna_seq = ''.join(bases)
-print("\n.............mRNA SEQUENCE.............")
+print ("\n.............mRNA SEQUENCE.............\n")
 print(rna_seq)
 
-protein = {"TTT": "F", "CTT": "L", "ATT": "I", "GTT": "V",
-           "TTC": "F", "CTC": "L", "ATC": "I", "GTC": "V",
-           "TTA": "L", "CTA": "L", "ATA": "I", "GTA": "V",
-           "TTG": "L", "CTG": "L", "ATG": "M", "GTG": "V",
-           "TCT": "S", "CCT": "P", "ACT": "T", "GCT": "A",
-           "TCC": "S", "CCC": "P", "ACC": "T", "GCC": "A",
-           "TCA": "S", "CCA": "P", "ACA": "T", "GCA": "A",
-           "TCG": "S", "CCG": "P", "ACG": "T", "GCG": "A",
-           "TAT": "Y", "CAT": "H", "AAT": "N", "GAT": "D",
-           "TAC": "Y", "CAC": "H", "AAC": "N", "GAC": "D",
-           "TAA": "STOP", "CAA": "Q", "AAA": "K", "GAA": "E",
-           "TAG": "STOP", "CAG": "Q", "AAG": "K", "GAG": "E",
-           "TGT": "C", "CGT": "R", "AGT": "S", "GGT": "G",
-           "TGC": "C", "CGC": "R", "AGC": "S", "GGC": "G",
-           "TGA": "STOP", "CGA": "R", "AGA": "R", "GGA": "G",
-           "TGG": "W", "CGG": "R", "AGG": "R", "GGG": "G"
+protein = {"UUU" : "F", "CUU" : "L", "AUU" : "I", "GUU" : "V",
+           "UUC" : "F", "CUC" : "L", "AUC" : "I", "GUC" : "V",
+           "UUA" : "L", "CUA" : "L", "AUA" : "I", "GUA" : "V",
+           "UUG" : "L", "CUG" : "L", "AUG" : "M", "GUG" : "V",
+           "UCU" : "S", "CCU" : "P", "ACU" : "T", "GCU" : "A",
+           "UCC" : "S", "CCC" : "P", "ACC" : "T", "GCC" : "A",
+           "UCA" : "S", "CCA" : "P", "ACA" : "T", "GCA" : "A",
+           "UCG" : "S", "CCG" : "P", "ACG" : "T", "GCG" : "A",
+           "UAU" : "Y", "CAU" : "H", "AAU" : "N", "GAU" : "D",
+           "UAC" : "Y", "CAC" : "H", "AAC" : "N", "GAC" : "D",
+           "UAA" : "STOP", "CAA" : "Q", "AAA" : "K", "GAA" : "E",
+           "UAG" : "STOP", "CAG" : "Q", "AAG" : "K", "GAG" : "E",
+           "UGU" : "C", "CGU" : "R", "AGU" : "S", "GGU" : "G",
+           "UGC" : "C", "CGC" : "R", "AGC" : "S", "GGC" : "G",
+           "UGA" : "STOP", "CGA" : "R", "AGA" : "R", "GGA" : "G",
+           "UGG" : "W", "CGG" : "R", "AGG" : "R", "GGG" : "G"
            }
 protein_sequence = ""
-start = 0
-end = 0
-for i in range(0, len(dna_seq)-(3+len(dna_seq) % 3), 3):
-    if protein[dna_seq[i:i+3]] == "STOP":
-        end = i
-        break
+start=0
+end=0
+j=0
+i=0
 
-for i in range(0, len(dna_seq)-(3+len(dna_seq) % 3), 3):
-    if protein[dna_seq[i:i+3]] == "M":
-        start = i
-        break
-if(start < end):
-    for i in range(start, end, 3):
-        if protein[dna_seq[i:i+3]] == "STOP":
-            break
-        protein_sequence += protein[dna_seq[i:i+3]]
-    print("\n.............PROTEIN SEQUENCE.............")
-    print(protein_sequence)
-else:
-    print("Protein Sequence does not exist")
+while(i<len(rna_seq)):
+  if i+3 < len(rna_seq):
+    if protein[rna_seq[i:i+3]] == "M" :
+        start=i
+        for j in range (start,len(rna_seq),3):
+          if j+3 < len(rna_seq):
+            if protein[rna_seq[j:j+3]] == "STOP" :
+               end=j
+               break
+
+        for k in range(start, end,3):
+            protein_sequence += protein[rna_seq[k:k+3]]
+        i=end
+  i+=1
+
+print ("\n.............PROTEIN SEQUENCE.............\n")
+print (protein_sequence)
